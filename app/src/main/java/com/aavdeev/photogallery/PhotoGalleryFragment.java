2edb4c8d9dd40d75b812f053b56e056a1c57bebb
Avdey87 <avdey87@gmail.com>
2017-10-25 21:39:53 +0300

    452 Упражнение. Gson

--- app/src/main/java/com/aavdeev/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/aavdeev/photogallery/PhotoGalleryFragment.java
@@ -11,12 +11,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PhotoGalleryFragment extends Fragment {
     private RecyclerView mPhotoRecyclerView;
     private static final String TAG = "PhotoGalleryFragment";
+    private List<GalleryItem> mItems = new ArrayList<>();
 
     public static PhotoGalleryFragment newInstance() {
         return new PhotoGalleryFragment();
@@ -25,6 +29,7 @@ public class PhotoGalleryFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+       //удержание фрагмента чтобы поворот не приводил к многократному созданию новых объектов AsyncTask
         setRetainInstance(true);
         new FetchItemsTask().execute();
     }
@@ -37,14 +42,84 @@ public class PhotoGalleryFragment extends Fragment {
         mPhotoRecyclerView = (RecyclerView) v
                 .findViewById(R.id.fragment_photo_gallery_recycle_view);
         mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        setupAdapter();
         return v;
     }
 
-    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+    //метод проверяет состояние списка объектов GalleryItem
+    private void setupAdapter() {
+        //проверка что фрагмент был присоеденен к активности
+        if (isAdded()) {
+            mPhotoRecyclerView.setAdapter(new PhotoAdapter(mItems));
+        }
+    }
+
+    //Класс удержатель для просмотра
+    private class PhotoHolder extends RecyclerView.ViewHolder {
+        private TextView mTitleTextView;
+
+        //Удержатель фото
+        public PhotoHolder(View itemView) {
+            super(itemView);
+            //в переменную записываем текст отображения
+            mTitleTextView = (TextView) itemView;
+        }
+
+        //мето для сыези картинки с текстом
+        public void bindGalleryItem(GalleryItem item) {
+            mTitleTextView.setText(item.toString());
+        }
+    }
+
+    // Класс переходник
+    private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder> {
+        private List<GalleryItem> mGalleryItems;
+
+        public PhotoAdapter(List<GalleryItem> galleryItems) {
+            mGalleryItems = galleryItems;
+        }
+
+        //Создаем тображение удержателя
         @Override
-        protected Void doInBackground(Void... params) {
-            new FlickrFetchr().fetchItems();
-            return null;
+        public PhotoHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            // в текст вью записываем текущую актиность
+            TextView textView = new TextView(getActivity());
+            //возращаем фото (ввиде текста)
+            return new PhotoHolder(textView);
+        }
+
+        //метод связка
+        @Override
+        public void onBindViewHolder(PhotoHolder photoHolder, int position) {
+            //устанавливаем текущую позицию картинки
+            GalleryItem galleryItem = mGalleryItems.get(position);
+            //связываем фото с позицией
+            photoHolder.bindGalleryItem(galleryItem);
+        }
+
+        //метод возвращает размер галлери фото
+        @Override
+        public int getItemCount() {
+            return mGalleryItems.size();
+        }
+    }
+
+
+    // третий параметр List<GalleryItem>> определяет тип результата AsyncTask он задает тип значений возвращаемого doInBackground(…), а также тип входного параметра onPostExecute(…).
+    private class FetchItemsTask extends AsyncTask<Void, Void, List<GalleryItem>> {
+
+        //возвращает список элементов GalleryItem
+        @Override
+        protected List<GalleryItem> doInBackground(Void... params) {
+            return new FlickrFetchr().fetchItems();
+        }
+
+        //метод обновляет поле mItems и вызвывает setupAdapter() после загрузки фото для обновления источника
+        //Этот метод получает список, загруженный в doInBackground(…), помещает его в mItems и обновляет адаптер RecyclerView.
+        @Override
+        protected void onPostExecute(List<GalleryItem> items) {
+            mItems = items;
+            setupAdapter();
         }
     }
 
